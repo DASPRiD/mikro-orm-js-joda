a404a5ac926f77ae2229b935992f4eeec91a141b
Ben Scholzen (DASPRiD) <mail@dasprids.de>
2024-02-07 16:17:44 +0100

    fix: manually convert database datetime value to Date

--- src/zoned-date-time-type.ts
+++ src/zoned-date-time-type.ts
@@ -1,29 +1,42 @@
-import { ZonedDateTime, convert, nativeJs } from "@js-joda/core";
+import { type ZonedDateTime, convert, nativeJs } from "@js-joda/core";
 import { type EntityProperty, type Platform, Type } from "@mikro-orm/core";
 
 export class ZonedDateTimeType extends Type<ZonedDateTime | null, Date | null> {
-    public convertToDatabaseValue(value: ZonedDateTime | Date | null): Date | null {
+    public convertToDatabaseValue(value: ZonedDateTime | null): Date | null {
         if (!value) {
             return null;
-        }
-
-        if (value instanceof Date) {
-            return value;
         }
 
         return convert(value).toDate();
     }
 
-    public convertToJSValue(value: ZonedDateTime | Date | null): ZonedDateTime | null {
-        if (!value) {
+    public convertToJSValue(
+        value: Date | string | number | null,
+        platform: Platform,
+    ): ZonedDateTime | null {
+        if (value === null) {
             return null;
         }
 
-        if (value instanceof ZonedDateTime) {
+        return nativeJs(this.databaseValueToDate(value, platform));
+    }
+
+    private databaseValueToDate(value: Date | string | number, platform: Platform): Date {
+        if (value instanceof Date) {
             return value;
         }
 
-        return nativeJs(value);
+        const tz = platform.getTimezone();
+
+        if (!tz || tz === "local") {
+            return new Date(value);
+        }
+
+        if (typeof value === "number" || value.includes("+")) {
+            return new Date(value);
+        }
+
+        return new Date(`${value}${tz}`);
     }
 
     public override compareAsType(): string {
